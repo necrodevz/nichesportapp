3af44308c97c17760aefa16f5b1ab264172eb00e
Vaibhav Gurnani <vaibhav@systango.com>
2017-05-25 16:39:22 +0530

    remove unused components from header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,13 +1,5 @@
 import React from 'react';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
-import IconMenu from 'material-ui/IconMenu';
-import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
-import FlatButton from 'material-ui/FlatButton';
-import NavigationExpandMoreIcon from 'material-ui/svg-icons/navigation/expand-more';
-import MenuItem from 'material-ui/MenuItem';
-import DropDownMenu from 'material-ui/DropDownMenu';
-import RaisedButton from 'material-ui/RaisedButton';
 import { Link } from 'react-router';
 import HeaderLink from './HeaderLink';
 import AppBar from 'material-ui/Appbar';
